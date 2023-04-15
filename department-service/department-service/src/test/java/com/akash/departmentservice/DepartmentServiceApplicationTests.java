d27847bb9ac3df37f00a3dbc7fd0201f1691748f
Akash <akashaher1512@gmail.com>
2023-04-15 14:27:22 +0530

    Department-service properties initialization

---
+++ department-service/department-service/src/test/java/com/akash/departmentservice/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.departmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
