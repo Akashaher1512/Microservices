cd01ce39be865273ffba0743defce834f22542d2
Akash <akashaher1512@gmail.com>
2023-04-15 14:45:06 +0530

    Department-service - Department entity , DepartmentRepository and DepartmentDto added

---
+++ department-service/department-service/src/main/java/com/akash/departmentservice/repository/DepartmentRepository.java
@@ -0,0 +1,8 @@
+package com.akash.departmentservice.repository;
+
+import com.akash.departmentservice.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department , Long> {
+
+}
